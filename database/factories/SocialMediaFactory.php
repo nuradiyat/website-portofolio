d4cfb91f3c79104b332adbd83e0e7aa1be459803
nuradiyat <mnuradiyat@gmail.com>
2026-06-29 22:37:45 +0700

    instlasi dan konfigurasi laravel dan filamament

---
+++ database/factories/SocialMediaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SocialMedia;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<SocialMedia>
+ */
+class SocialMediaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
