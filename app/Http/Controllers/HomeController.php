b193612ab929ecba6e744ba621471ab9607cb1a4
nuradiyat <mnuradiyat@gmail.com>
2026-07-01 23:50:50 +0700

    update controller homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,40 @@
-public function index()
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Certificate;
+use App\Models\Experience;
+use App\Models\Profile;
+use App\Models\Project;
+use App\Models\Skill;
+use App\Models\SocialMedia;
+use App\Models\Testimonial;
+
+class HomeController extends Controller
 {
-$skills = Skill::all();
+    public function index()
+    {
+        $skills = Skill::all();
 
-$projects = Project::latest()
-->take(6)
-->get();
+        $projects = Project::latest()->take(6)->get();
 
-$certificates = Certificate::latest()
-->take(6)
-->get();
+        $certificates = Certificate::latest()->take(6)->get();
 
-$experiences = Experience::latest()
-->take(6)
-->get();
+        $experiences = Experience::latest()->take(6)->get();
 
-$testimonials = Testimonial::latest()
-->take(6)
-->get();
+        $testimonials = Testimonial::latest()->take(6)->get();
 
-return view('home', compact(
-'skills',
-'projects',
-'certificates',
-'experiences',
-'testimonials'
-));
+        return view('home', [
+            'skills' => $skills,
+            'projects' => $projects,
+            'certificates' => $certificates,
+            'experiences' => $experiences,
+            'testimonials' => $testimonials,
+
+            'projectCount' => Project::count(),
+            'certificateCount' => Certificate::count(),
+            'experienceCount' => Experience::count(),
+            'skillCount' => Skill::count(),
+        ]);
+    }
 }
